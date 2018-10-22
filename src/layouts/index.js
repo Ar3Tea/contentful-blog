0ccdb344e38aabcef19d0152aa8c23afd3daad3e
Ar3Tea <tbusinessr@gmail.com>
2018-10-22 17:07:38 -0400

    testing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -31,9 +31,9 @@ const Template = ({ children }) => {
         </div>
       </ThemeProvider>
       {/* Footer placed in seperate ThemeProvider to avoid Rendering an extra DIV in HTML output  */}
-      <ThemeProvider theme={theme}>
+      {/*<ThemeProvider theme={theme}>
         <Footer />
-      </ThemeProvider>
+      </ThemeProvider>*/}
     </div>
   )
 }
